30273e6b23aaed87680de568493735b664685f29
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-06-20 21:24:02 -0400

    Render Main section

--- src/app.js
+++ src/app.js
@@ -1,64 +1,70 @@
-import Weather from "./weather.js";
+import Weather from './weather.js';
 
-const app = (() =>{
-    const key = 'MWR6Z6RGNMP6WH5SP9P2S997P';
+const app = (() => {
+   const key = 'MWR6Z6RGNMP6WH5SP9P2S997P';
+   let currentWeather = null;
 
-    const defaultWeather = null;
-    let currentWeather = defaultWeather;
+   const getCurrentWeather = () => {
+      return currentWeather;
+   };
+   const fetchWeather = async (location) => {
+      const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`;
+      try {
+         const response = await fetch(url);
+         const data = await response.json();
 
-    const getCurrentWeather = () => {return currentWeather};
-    const fetchWeather = async (location) => {
-        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`;
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            
-            console.log(data);
+         return data;
+      } catch (error) {
+         console.error(error);
+      }
+   };
 
-            return data;
-        } catch (error) {
-            console.error(error);
-        }
-    }
+   const parseWeather = (data) => {
+      const location = data.resolvedAddress;
+      const temp = {
+         current: data.currentConditions.temp,
+         feelsLike: data.currentConditions.feelsLike,
+         min: data.days[0].tempmin,
+         max: data.days[0].tempmax,
+      };
+      const atmosphere = {
+         humidity: data.currentConditions.humidity,
+         pressure: data.currentConditions.pressure,
+         uvIndex: data.currentConditions.uvindex,
+      };
+      const wind = {
+         speed: data.currentConditions.windspeed,
+         gust: data.currentConditions.windgust,
+      };
+      const sun = {
+         sunrise: data.currentConditions.sunrise,
+         sunset: data.currentConditions.sunset,
+      };
+      const precipitation = {
+         precipType: data.currentConditions.preciptype,
+         precipProb: data.currentConditions.precipprob,
+         precip: data.currentConditions.precip,
+      };
+      const condition = data.currentConditions.conditions;
+      const icon = data.currentConditions.icon;
+      const desc = data.days[0].description;
 
-    const parseWeather = async (weatherData) => {
-        const data = await weatherData;
-        const location = data.resolvedAddress;
-        const temp = {
-            current: data.currentConditions.temp,
-            feelsLike: data.currentConditions.feelsLike,
-            min: data.days[0].tempmin,
-            max: data.days[0].tempmax,
-        };
-        const atmosphere = {
-            humidity: data.currentConditions.humidity,
-            pressure: data.currentConditions.pressure,
-            uvIndex: data.currentConditions.uvindex
-        };
-        const wind = {
-            speed: data.currentConditions.windspeed,
-            gust: data.currentConditions.windgust,
-        };
-        const sun = {
-            sunrise: data.currentConditions.sunrise,
-            sunset: data.currentConditions.sunset,
-        }
-         const precipitation = {
-            precipType: data.currentConditions.preciptype,
-            precipProb: data.currentConditions.precipprob,
-            precip: data.currentConditions.precip
-        };
-        const condition = data.currentConditions.conditions;
-        const icon = data.currentConditions.icon;
-        const desc = data.days[0].description;
+      const weather = new Weather(
+         location,
+         temp,
+         atmosphere,
+         wind,
+         sun,
+         precipitation,
+         condition,
+         icon,
+         desc,
+      );
+      currentWeather = weather;
 
-        const weather = new Weather(location, temp, atmosphere, wind, sun, precipitation, condition, icon, desc);
-        currentWeather = weather;
-
-        console.log(currentWeather);
-        
-    }
-    return {fetchWeather, parseWeather, getCurrentWeather};
+      console.log(currentWeather);
+   };
+   return { fetchWeather, parseWeather, getCurrentWeather };
 })();
 
 export default app;
--- src/weather.js
+++ src/weather.js
@@ -1,32 +1,42 @@
 export default class Weather {
-    constructor (location, temp, atmosphere, wind, sun, precipitation, conditions, icon, description){
-        this.location = location;
-        this.temperature = {
-            current: temp.current,
-            feelsLike: temp.feelsLike,
-            min: temp.min,
-            max: temp.max,
-        };
-        this.atmosphere = {
-            humidity: atmosphere.humidity,
-            pressure: atmosphere.pressure,
-            uvIndex: atmosphere.uvIndex,
-        };
-        this.wind = {
-            speed: wind.speed,
-            gust: wind.gust,
-        };
-        this.sun = {
-            sunrise: sun.sunrise,
-            sunset: sun.sunset,
-        };
-        this.precipitation = {
-            precipType: precipitation.precipType,
-            precipProb: precipitation.precipProb,
-            precip: precipitation.precip
-        };
-        this.conditions = conditions;
-        this.icon = icon;
-        this.description = description;
-    }
+   constructor(
+      location,
+      temp,
+      atmosphere,
+      wind,
+      sun,
+      precipitation,
+      conditions,
+      icon,
+      description,
+   ) {
+      this.location = location.charAt(0).toUpperCase() + location.slice(1);
+      this.temperature = {
+         current: temp.current,
+         feelsLike: temp.feelsLike,
+         min: temp.min,
+         max: temp.max,
+      };
+      this.atmosphere = {
+         humidity: atmosphere.humidity,
+         pressure: atmosphere.pressure,
+         uvIndex: atmosphere.uvIndex,
+      };
+      this.wind = {
+         speed: wind.speed,
+         gust: wind.gust,
+      };
+      this.sun = {
+         sunrise: sun.sunrise,
+         sunset: sun.sunset,
+      };
+      this.precipitation = {
+         precipType: precipitation.precipType,
+         precipProb: precipitation.precipProb,
+         precip: precipitation.precip,
+      };
+      this.conditions = conditions;
+      this.icon = icon;
+      this.description = description;
+   }
 }
